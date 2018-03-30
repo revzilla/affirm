be6e1588a7f29ce44b7a5629a14e50dfc78625e9
Tyler Cain <tyler.cain@revzilla.com>
2018-03-30 12:58:13 -0400

    include jason in applications, not poison

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Affirm.Mixfile do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.1.1"
 
   def project do
     [app: :affirm,
@@ -17,7 +17,7 @@ defmodule Affirm.Mixfile do
   end
 
   def application do
-    [applications: [:logger, :httpoison, :poison]]
+    [applications: [:logger, :httpoison, :jason]]
   end
 
   defp description do
