8b00924e7e8ded9d7c11d5428792a4e8ed841036
Steve DeGele <steve.degele@revzilla.com>
2018-08-03 17:58:50 -0400

    bump httpoison

--- mix.exs
+++ mix.exs
@@ -1,19 +1,21 @@
 defmodule Affirm.Mixfile do
   use Mix.Project
 
-  @version "0.1.1"
+  @version "0.1.2"
 
   def project do
-    [app: :affirm,
-     version: @version,
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     test_coverage: [tool: ExCoveralls],
-     description: description(),
-     package: package(),
-     name: "Affirm",
-     deps: deps()]
+    [
+      app: :affirm,
+      version: @version,
+      elixir: "~> 1.3",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      test_coverage: [tool: ExCoveralls],
+      description: description(),
+      package: package(),
+      name: "Affirm",
+      deps: deps()
+    ]
   end
 
   def application do
@@ -27,15 +29,17 @@ defmodule Affirm.Mixfile do
   end
 
   defp package do
-    [maintainers: ["RevZilla", "Tyler Cain", "Steve DeGele"],
-     licenses: ["MIT"],
-     links: %{"GitHub" => "https://github.com/revzilla/affirm"},
-     files: ~w(lib mix.exs README.md)]
+    [
+      maintainers: ["RevZilla", "Tyler Cain", "Steve DeGele"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/revzilla/affirm"},
+      files: ~w(lib mix.exs README.md)
+    ]
   end
 
   defp deps do
     [
-      {:httpoison, "~> 0.12"},
+      {:httpoison, "~> 1.1.1"},
       {:jason, "~> 1.0"},
       {:bypass, "~> 0.8", only: :test},
       {:excoveralls, "~> 0.7.4", only: :test},
